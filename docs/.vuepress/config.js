c6e9ba3cd1d3603513a909fcdc4013c45949cd9c
liuhongda <1156614427@qq.com>
2023-10-19 16:05:29 +0800

    添加笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,24 @@
 import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "vuepress";
 
 export default defineUserConfig({
   base: "/lin-note/",
   lang: "zh-CN",
-  title: "你好， VuePress ！",
-  description: "这是我的第一个 VuePress 站点",
+  title: "笔记",
+  description: "ldlang-note",
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "git笔记",
+        link: "/git-note/",
+      },
+    ],
+    sidebar: {
+      '/git-note/': ['README.md']
+    },
+  }),
 });
--- docs/README.md
+++ docs/README.md
@@ -1,13 +1,9 @@
 ---
 home: true
-heroText: 标题
+heroText: 学海无涯，苦乐自知
 tagline: 副标题
-features:
-  - title: 简洁至上
-    details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-  - title: Vue驱动
-    details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-  - title: 高性能
-    details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: Copyright © xx
+actions:
+  - text: git
+    link: git-note/ # 修改处
+    type: primary
 ---
