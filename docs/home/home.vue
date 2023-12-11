d6ea4f45a5668b4db8423a1a3c1843d2bced799d
lindalang <1156614427@qq.com>
2023-12-11 21:29:35 +0800

    调整笔记

--- docs/home/home.vue
+++ docs/home/home.vue
@@ -14,10 +14,23 @@ import canvasStar from "./components/canvas-star.vue";
 import Typed from "typed.js";
 import { onMounted, onBeforeUnmount } from "vue";
 
+const wisdomList = [
+  "相信自己，你能做到任何事情。",
+  "积学如山，心知其意。",
+  "只要路是对的，就不怕路远。",
+  "苦读作舟，乐学为径。",
+  "不要轻易放弃，成功就在拐角处。",
+  "恒心是通往成功的桥梁。",
+  "锲而不舍，金石可镂。",
+  "没有无法越过的山峰，没有无法跨过的河流。",
+  "学海无涯，苦乐自知。",
+  "众里寻他千百度，蓦然回首，那人却在灯火阑珊处。"
+]
+
 let typed;
 onMounted(() => {
   typed = new Typed("#title", {
-    strings: ["学海无涯，苦乐自知。"],
+    strings: [wisdomList[parseInt(Math.random() * 10)]],
     typeSpeed: 50,
   });
 });
